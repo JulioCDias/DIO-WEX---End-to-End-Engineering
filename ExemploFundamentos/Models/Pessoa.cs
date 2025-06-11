e5c1869fc9ebfd5fb431a747c56025f657ebaa42
Julio Cesar <juliozulu2008@gmail.com>
2025-06-11 12:23:51 -0300

    Correção da convenção de codigo

---
+++ ExemploFundamentos/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Models // Namespace é uma forma de organizar o código em C#
+{
+    public class Pessoa // Classse é um modelo que define as propriedades e comportamentos de um objeto
+    {
+        // Propriedades são características do objeto, como Nome e Idade
+        public string? Nome { get; set; } // Propriedade Nome do tipo string
+        public int Idade { get; set; } // Propriedade Idade do tipo int
+
+        public void Apresentar() // Método para apresentar a pessoa
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
+        }
+    }
+}
---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,7 @@
+﻿using ExemploFundamentos.Models; // Importando o namespace que contém a classe Pessoa
+// Início do programa principal
+
+Pessoa pessoa = new Pessoa(); // Instanciando a classe Pessoa
+pessoa.Nome = "João"; // Atribuindo o nome
+pessoa.Idade = 30; // Atribuindo a idade
+pessoa.Apresentar(); // Chamando o método para apresentar a pessoa
